e51a7296de85979ec13b95cd27fe3e0cd175a4a2
YSGoh <yongseng1207@gmail.com>
2018-08-25 16:55:23 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+    import MyChat from "./src/MyChat";
+
+    export default class App extends Component {
+      render() {
+        return <MyChat />;
+      }
+    }
